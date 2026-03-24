317040a1145ee296aadb24b8b187cc5ef33a9a7c
Gautham <korrapatigautham@gmail.com>
2026-03-24 15:20:38 -0700

    usememo caching and firestore update for db holding

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,77 +1,117 @@
 "use client";
 
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useEffect, useMemo, useState } from "react";
+import { doc, DocumentData, getDoc } from "firebase/firestore";
+import { firestore } from "@/utils/firebase/firebaseConfig";
 import Select from "react-select";
-
 interface Professor {
     department: string,
     professor_name: string,
     email: string
 }
 
-// interface DropdownSelectOptions {
-//     department: string,
-//     professor_name: string
-// }
-
 const Search: React.FC = () => {
-    const [professorList, setProfessorList] = useState<Professor[]>([
-        // {
-        // department: "type",
-        // professor_name: "shit",
-        // email: "neheh"
-        // }
-    ]);
+    const [professorList, setProfessorList] = useState<Professor[]>([]);
     const [selectedOption, setSelectedOption] = useState<{value: string, label: string} | null>(null);
-    const professor_name_list = [];
+
+    const [fetchRating, setFetchRating] = useState<{id: string, data: DocumentData} | null>(null);
+    const db = firestore;
+    const collectionName = "wrd";
+    const documentId = "wrd";
+
+
+    // { collectionName, documentId}: {collectionName: string, documentId: string}
 
     useEffect(() => {
         const setProfessors = async () => {
-            setProfessorList([
-                {
-                    department: "type shit",
-                    professor_name: "shit",
-                    email: "type"
-                },
-                {
-                    department: "yippe",
-                    professor_name: "wrd",
-                    email: "hehe"
-                }
-            ])
-
-            return null;
+            try {
+                setProfessorList([
+                    {
+                        department: "type shit",
+                        professor_name: "shit",
+                        email: "type"
+                    },
+                    {
+                        department: "yippe",
+                        professor_name: "wrd",
+                        email: "hehe"
+                    }
+                ])
+            }
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            catch(error: any) {
+                console.log(error?.message);
+            }            
         }
-        setProfessors();
-        // alert(professorList);
-    }, []);
 
-    for (let i = 0; i < professorList.length; i++) {
-        professor_name_list.push({
-            value: professorList[i].department,
-            label: professorList[i].professor_name
-        });
-    }
+        const fetchDocument = async () => {
+                try {
+                    const docRef = doc(db, collectionName, documentId);
+                    const docSnap = await getDoc(docRef);
+
+                    if (docSnap.exists()) {
+                        console.log("name CHECK: ", docSnap.data());
+                        setFetchRating({id: docSnap.id, data: docSnap.data()});
+                    }
+                    else {
+                        setFetchRating(null);
+                    }
+                } catch (error) {
+                    console.error("Error fetching document: ", error);
+                }
+            }
+
+            // need to do in async way for async methods
+            const setAllData = async () => {
+                await setProfessors();
+                await fetchDocument();
+            }
+
+            setAllData();
+
+    }, [db]);
+
     
+    const new_professor_name_list = useMemo(
+        () => {
+            const result = [];
+            for (let i = 0; i < professorList.length; i++) {
+                console.log("department: ", professorList[i].department)
+                result.push({
+                    value: professorList[i].department,
+                    label: professorList[i].professor_name
+                });
+            }      
+            
+            return result;
+        },
+        [professorList]
+    );
 
-    // console.log("PROFESSOR LIST: " + professorList[0]);
+    // const professor_name_list = useMemo(() => 
+    //     professorList.map(prof => ({
+    //         value: prof.department,
+    //         label: prof.professor_name
+    //     })), [professorList]);   
+    /*
+    interface object with department, professor
+    useEffect(() => {
+        axios.get("")
+        .then(response => {
+            setProfessorList(() => (
+                // ...prev,
+                response.data
+            ))
+        })
+        .catch(error => {
+            console.error(error);
+            alert(error.message);
+        })
+    }, []);
+    */
 
-    // interface object with department, professor
-    // useEffect(() => {
-    //     axios.get("")
-    //     .then(response => {
-    //         setProfessorList(() => (
-    //             // ...prev,
-    //             response.data
-    //         ))
-    //     })
-    //     .catch(error => {
-    //         console.error(error);
-    //         alert(error.message);
-    //     })
-    // }, []);
+    // from professor, retrieve RMP + AOC ratemy from firestore
 
     return (
         <>
@@ -83,9 +123,25 @@ const Search: React.FC = () => {
             />
             <div>
                 <p>Pick a professor...</p>
-                <Select className="text-black" options={professor_name_list} onChange={setSelectedOption} defaultValue={selectedOption} />
-                {/* from api, get metrics regarding professor, reviews, etc */}
-            </div>            
+                <Select className="text-black" options={new_professor_name_list} onChange={setSelectedOption} defaultValue={selectedOption} />
+                {/* from db, get metrics regarding professor, reviews, etc with rmp tab and AOC tab, if no ratemy exists, redirect to login page if they want to make rating */}
+
+                    Generate Professor Ratings
+
+                    {
+                        (selectedOption!==null) ? (
+                            <>
+                                <p>{fetchRating?.id}</p>
+                                {console.log(fetchRating)}
+                            </>
+                        ) : (
+                            <div>
+                                <p>tu madre</p>
+                            </div>
+                        )
+                    }
+                    
+            </div>
         </>
     )
 }
--- src/utils/firebase/firebaseConfig.ts
+++ src/utils/firebase/firebaseConfig.ts
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics, isSupported } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,6 +21,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const firestore = getFirestore(app);
 let analytics;
 
 isSupported().then((supported) => {
@@ -33,4 +35,4 @@ isSupported().then((supported) => {
   console.error("Error checking firebase anlaytics", error);
 })
 
-export {app, auth, analytics};
+export {app, auth, analytics, firestore};
